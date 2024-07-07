79f15298ebfbdc12ac08ba0ec513324dd98049d3
udaysaimanoj123 <117463621+udaysaimanoj123@users.noreply.github.com>
2024-07-07 18:57:02 +0530

    fc

---
+++ src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
+
+const AppDownload = () => {
+    return (
+        <div className='app-download' id='app-download'>
+            
+        </div>
+    )
+}
+
+export default AppDownload
